ca3ab38f9dddf76ddaf5554b34d31ac0d626503f
Lucas Regio <souzaregiolucas@gmail.com>
2021-10-17 14:34:58 -0300

    New Services

--- src/main/java/com/woods/tcc/services/BudgetService.java
+++ src/main/java/com/woods/tcc/services/BudgetService.java
@@ -3,33 +3,33 @@ package com.woods.tcc.services;
 import java.util.List;
 import java.util.Optional;
 
-import com.woods.tcc.repositories.AddressRepository;
+import com.woods.tcc.repositories.BudgetRepository;
 import com.woods.tcc.services.exceptions.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
-import com.woods.tcc.model.Address;
+import com.woods.tcc.model.Budget;
 
 @Service
-public class AddressService {
+public class BudgetService {
   @Autowired
-  AddressRepository addressRepository;
+  BudgetRepository addressRepository;
 
-  public List<Address> findAll() {
+  public List<Budget> findAll() {
     return this.addressRepository.findAll();
   }
 
-  public Address findById(Long id) {
-    Optional<Address> service = this.addressRepository.findById(id);
+  public Budget findById(Long id) {
+    Optional<Budget> service = this.addressRepository.findById(id);
     return service.orElseThrow();
   }
-  public Address createAddress(Address entity){
+  public Budget createBudget(Budget entity){
     return this.addressRepository.save(entity);
   }
 
-  public void deleteAddress (long id){
+  public void deleteBudget (long id){
     try {
       this.addressRepository.deleteById(id);
     } catch(EmptyResultDataAccessException e) {
@@ -37,9 +37,9 @@ public class AddressService {
     }
   }
 
-  public Address updateAddress (Long id, Address obj){
+  public Budget updateBudget (Long id, Budget obj){
     try {
-      Address entity = this.addressRepository.getById(id);
+      Budget entity = this.addressRepository.getById(id);
       updateData(entity, obj);
       return this.addressRepository.save(entity);
     } catch (EntityNotFoundException e) {
@@ -47,12 +47,12 @@ public class AddressService {
     }
   }
 
-  public void updateData(Address entity, Address obj) {
-    entity.setStreet(obj.getStreet());
-    entity.setNumber(obj.getNumber());
-    entity.setComplement(obj.getComplement());
-    entity.setDistrict(obj.getDistrict());
-    entity.setCity(obj.getCity());
-    entity.setState(obj.getState());
+  public void updateData(Budget entity, Budget obj) {
+    entity.setName(obj.getName());
+    entity.setDescription(obj.getDescription());
+    entity.setValue(obj.getValue());
+    entity.setExpirationDate(obj.getExpirationDate());
+    entity.setRequestDate(obj.getRequestDate());
   }
+
 }
